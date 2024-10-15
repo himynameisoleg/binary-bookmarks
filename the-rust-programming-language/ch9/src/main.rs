6299d2008ff7da809efb6b4a5231dd5135918509
Oleg <heyitsoleg@gmail.com>
2024-10-15 08:32:22 -0500

    recoverable errors

--- the-rust-programming-language/ch9/src/main.rs
+++ the-rust-programming-language/ch9/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+
 fn main() {
     // Error Handling
     // 2 types of errors: Recoverable and Unrecoverable
@@ -5,6 +7,16 @@ fn main() {
     // Recoverable - Result<T, E> enum
 
     // panic!("crash and burn");
-    let v = vec![1, 2, 3];
-    v[99];
+    //run with  RUST_BACKTRACE=1 cargo run to see full stack trace
+    // let v = vec![1, 2, 3];
+    // v[99];
+
+    // Recoverable Errors with Result
+    let greeting_file_result = File::open("hello.txt");
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    };
 }
