7137aa7e88cced40162d014f9115b791b14a8543
Oleg <heyitsoleg@gmail.com>
2024-10-16 08:23:53 -0500

    2 kinds of match syntax for handling Result<T, E>

--- the-rust-programming-language/ch9/src/main.rs
+++ the-rust-programming-language/ch9/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     // Error Handling
@@ -12,11 +13,39 @@ fn main() {
     // v[99];
 
     // Recoverable Errors with Result
-    let greeting_file_result = File::open("hello.txt");
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => {
+    // let greeting_file_result = File::open("hello.txt");
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => {
+    //         panic!("Problem opening the file: {:?}", error);
+    //     }
+    // };
+
+    // Handling Different Kinds of Errors
+    // let greeting_file_result = File::open("hello.txt");
+    // let _greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => {
+    //                 panic!("Tried to create file but there was a problem: {:?}", e);
+    //             }
+    //         },
+    //         other_error => {
+    //             panic!("Problem opening the file: {:?}", other_error);
+    //         }
+    //     },
+    // };
+
+    // Alternative to the above
+    let _greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Tried to create file but there was a problem: {:?}", error);
+            })
+        } else {
             panic!("Problem opening the file: {:?}", error);
         }
-    };
+    });
 }
