54c83f1d1409c24141272ecad296b8bd3e603bd0
Oleg <heyitsoleg@gmail.com>
2024-10-21 08:22:23 -0500

    ? shortcut with Option return type

--- the-rust-programming-language/ch9/src/main.rs
+++ the-rust-programming-language/ch9/src/main.rs
@@ -81,9 +81,14 @@ fn main() {
     // }
 
     // Even shorter way
-    fn read_username_from_file() -> Result<String, io::Error> {
-        let mut username = String::new();
-        File::open("hello.txt")?.read_to_string(&mut username)?;
-        Ok(username)
+    // fn read_username_from_file() -> Result<String, io::Error> {
+    //     let mut username = String::new();
+    //     File::open("hello.txt")?.read_to_string(&mut username)?;
+    //     Ok(username)
+    // }
+
+    // Using ? with Option
+    fn last_char_of_first_line(text: &str) -> Option<char> {
+        text.lines().next()?.chars().last()
     }
 }
