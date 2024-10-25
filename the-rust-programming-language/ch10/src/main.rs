335e65f8f382273e4099007230b3f86a45d77f80
Oleg <heyitsoleg@gmail.com>
2024-10-25 15:14:40 -0500

    gnerics in method definitions

--- the-rust-programming-language/ch10/src/main.rs
+++ the-rust-programming-language/ch10/src/main.rs
@@ -1,23 +1,89 @@
-fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
+// fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+//     let mut largest = &list[0];
+//
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//
+//     largest
+// }
+//
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+//     let result = largest(&number_list);
+//
+//     println!("The largest number is {}", result);
+//
+//     let char_list = vec!['y', 'm', 'a', 'q'];
+//     let result = largest(&char_list);
+//
+//     println!("The largest number is {result}");
+// }
 
-    for item in list {
-        if item > largest {
-            largest = item;
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
+//
+// enum Option<T> {
+//     Some(T),
+//     None,
+// }
+//
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
+//
+// fn main() {
+//     let integer = Point { x: 1, y: 10 };
+//     let float = Point { x: 1.0, y: 4.0 };
+//     let mixed = Point { x: 1, y: 4.0 };
+// }
+
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+//
+// impl<T> Point<T> {
+//     fn x(&self) -> &T {
+//         &self.x
+//     }
+// }
+//
+// impl Point<f32> {
+//     fn distance_from_origin(&self) -> f32 {
+//         (self.x.powi(2) + self.y.powi(2)).sqrt()
+//     }
+// }
+//
+// fn main() {
+//     let p = Point { x: 5, y: 10 };
+//     println!("p.x = {}", p.x());
+// }
+
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
         }
     }
-
-    largest
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
 
-    println!("The largest number is {}", result);
+    let p3 = p1.mixup(p2);
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = largest(&char_list);
-
-    println!("The largest number is {result}");
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
