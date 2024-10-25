fc6f4a578647ff9fc9290cee56a0599a55ed9d30
Oleg <heyitsoleg@gmail.com>
2024-10-25 14:56:10 -0500

    generic data types

--- the-rust-programming-language/ch10/src/main.rs
+++ the-rust-programming-language/ch10/src/main.rs
@@ -1,16 +1,4 @@
-fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
-
-    println!("The largest number is {}", result);
-
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
-
-    println!("The largest number is {}", result);
-}
-
-fn largest(list: &[i32]) -> &i32 {
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
@@ -20,4 +8,16 @@ fn largest(list: &[i32]) -> &i32 {
     }
 
     largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+
+    println!("The largest number is {result}");
 }
