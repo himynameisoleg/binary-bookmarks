31aab8e1e012f88b83ceda2a75a27de612ac15e4
Oleg <heyitsoleg@gmail.com>
2024-11-04 21:59:10 -0600

    testing panics

--- the-rust-programming-language/ch11/src/lib.rs
+++ the-rust-programming-language/ch11/src/lib.rs
@@ -73,9 +73,39 @@
 //     }
 // }
 
-pub fn greeting(name: &str) -> String {
-    // format!("Hello {name}!")
-    String::from("HELLO")
+// pub fn greeting(name: &str) -> String {
+//     // format!("Hello {name}!")
+//     String::from("HELLO")
+// }
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn greeting_contains_name() {
+//         let result = greeting("Carl");
+//         assert!(
+//             result.contains("Carl"),
+//             "Greeting did not contain name, value was `{result}`"
+//         );
+//     }
+// }
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!("Guess value mus be >= 1, got {}", value);
+        } else if value > 100 {
+            panic!("Guess value must be <= 100, got {}", value)
+        }
+
+        Guess { value }
+    }
 }
 
 #[cfg(test)]
@@ -83,11 +113,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Carl");
-        assert!(
-            result.contains("Carl"),
-            "Greeting did not contain name, value was `{result}`"
-        );
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
