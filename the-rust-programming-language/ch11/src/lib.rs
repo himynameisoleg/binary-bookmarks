dd0a13de5134cbf7981537e7c852d7f6e3d338db
Oleg <heyitsoleg@gmail.com>
2024-11-05 20:32:26 -0600

    specifying or ignoring tests

--- the-rust-programming-language/ch11/src/lib.rs
+++ the-rust-programming-language/ch11/src/lib.rs
@@ -119,14 +119,47 @@
 //     }
 // }
 
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() -> Result<(), String> {
+//         if 2 + 2 == 4 {
+//             Ok(())
+//         } else {
+//             Err(String::from("two plus two does not equal 4"))
+//         }
+//     }
+// }
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() -> Result<(), String> {
-        if 2 + 2 == 4 {
-            Ok(())
-        } else {
-            Err(String::from("two plus two does not equal 4"))
-        }
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn on_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+
+    // single test with full test name: `cargo test test_two_and_two`
+    // multiple tests based on name: `cargo test add`
+
+    #[test]
+    #[ignore = "long running test, include with `cargo test -- --ignored`"]
+    fn expensive_test() {
+        assert_eq!(true, true);
     }
 }
