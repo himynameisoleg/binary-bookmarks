89b17b1b53a40bf2208feb3e15298a65848cbb2d
Oleg <heyitsoleg@gmail.com>
2024-11-09 13:22:26 -0600

    refactor with closure

--- the-rust-programming-language/ch13/src/main.rs
+++ the-rust-programming-language/ch13/src/main.rs
@@ -9,23 +9,22 @@ fn main() {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
+    // let expensive_result = simlulated_expensive_calculation(intensity);
+    // closure type annotation is not necesary: eg. `let s = closure |x| x;`
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
     if intensity < 25 {
-        println!(
-            "Today, do {} pushups!",
-            simlulated_expensive_calculation(intensity)
-        );
-        println!(
-            "Next, do {} situps!",
-            simlulated_expensive_calculation(intensity)
-        );
+        println!("Today, do {} pushups!", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_closure(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Stay hydrated!");
         } else {
-            println!(
-                "Today, run for {} mins!",
-                simlulated_expensive_calculation(intensity)
-            );
+            println!("Today, run for {} mins!", expensive_closure(intensity));
         }
     }
 }
