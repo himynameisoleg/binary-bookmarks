e2abcdad0e0f0e10a67a6936737701249e93a34b
Oleg <heyitsoleg@gmail.com>
2024-11-09 13:34:51 -0600

    add Catcher struct for memoizing the expensive value after calculation

--- the-rust-programming-language/ch13/src/main.rs
+++ the-rust-programming-language/ch13/src/main.rs
@@ -8,23 +8,51 @@ fn main() {
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
+struct Catcher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Catcher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Catcher<T> {
+        Catcher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
 fn generate_workout(intensity: u32, random_number: u32) {
-    // let expensive_result = simlulated_expensive_calculation(intensity);
-    // closure type annotation is not necesary: eg. `let s = closure |x| x;`
-    let expensive_closure = |num| {
+    let mut expensive_result = Catcher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
 
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_closure(intensity));
-        println!("Next, do {} situps!", expensive_closure(intensity));
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Stay hydrated!");
         } else {
-            println!("Today, run for {} mins!", expensive_closure(intensity));
+            println!("Today, run for {} mins!", expensive_result.value(intensity));
         }
     }
 }
