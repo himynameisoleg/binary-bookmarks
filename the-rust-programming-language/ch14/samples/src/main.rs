862ccce3d389fc7179758a06a601ec276efd6533
Oleg <heyitsoleg@gmail.com>
2024-11-14 08:21:17 -0600

    exporting crates for easier consuption and clear docs

--- the-rust-programming-language/ch14/samples/src/main.rs
+++ the-rust-programming-language/ch14/samples/src/main.rs
@@ -1,3 +1,8 @@
+use samples::mix;
+use samples::PrimaryColor;
+
 fn main() {
-    println!("Hello, world!");
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+    mix(red, yellow);
 }
