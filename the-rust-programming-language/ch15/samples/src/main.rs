112dcb6b933e93014e915d3f58bcc13b8ace7a00
Oleg <heyitsoleg@gmail.com>
2024-11-19 08:20:53 -0600

    demo of Drop trait being automagically called
    * called once each var moved out of scope

--- the-rust-programming-language/ch15/samples/src/main.rs
+++ the-rust-programming-language/ch15/samples/src/main.rs
@@ -14,37 +14,56 @@
 //     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 // }
 
-use std::ops::Deref;
+// use std::ops::Deref;
+//
+// struct MyBox<T>(T);
+//
+// impl<T> MyBox<T> {
+//     fn new(x: T) -> MyBox<T> {
+//         MyBox(x)
+//     }
+// }
+//
+// impl<T> Deref for MyBox<T> {
+//     type Target = T;
+//
+//     fn deref(&self) -> &T {
+//         &self.0
+//     }
+// }
+// fn main() {
+//     // let x = 5;
+//     // let y = MyBox::new(x);
+//     //
+//     // assert_eq!(5, x);
+//     // assert_eq!(5, *y); // *(y.deref())
+//
+//     let m = MyBox::new(String::from("Rust"));
+//     hello(&m);
+//     // because of Deref coersion we turn &MyBox<String> into &String into &str
+//     // otherwise we would need to explicitly dereference with `hello(&(*m)[..];`
+//     // to match all of the signitures
+// }
+//
+// fn hello(name: &str) {
+//     println!("Hello, {}!", name);
+// }
 
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
+// Code Cleanup with `Drop` trait
+struct CustomSmartPointer {
+    data: String,
 }
-
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.0
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
     }
 }
 fn main() {
-    // let x = 5;
-    // let y = MyBox::new(x);
-    //
-    // assert_eq!(5, x);
-    // assert_eq!(5, *y); // *(y.deref())
-
-    let m = MyBox::new(String::from("Rust"));
-    hello(&m);
-    // because of Deref coersion we turn &MyBox<String> into &String into &str
-    // otherwise we would need to explicitly dereference with `hello(&(*m)[..];`
-    // to match all of the signitures
-}
-
-fn hello(name: &str) {
-    println!("Hello, {}!", name);
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointer created.");
 }
