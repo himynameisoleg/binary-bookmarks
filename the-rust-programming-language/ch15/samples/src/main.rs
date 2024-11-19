f1ee243448d925dafc6b62c84d7b37d1d2b7a46c
Oleg <heyitsoleg@gmail.com>
2024-11-18 19:25:59 -0600

    implement `Deref` trait

--- the-rust-programming-language/ch15/samples/src/main.rs
+++ the-rust-programming-language/ch15/samples/src/main.rs
@@ -1,15 +1,40 @@
-use crate::List::{Cons, Nil};
+// use crate::List::{Cons, Nil};
+//
+// // Cons (construct functions) used in functional languages as recursive types
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
+//
+// fn main() {
+//     // this isnt really useful
+//     // let b = Box::new(5);
+//     // println!("b = {}", b);
+//
+//     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+// }
 
-// Cons (construct functions) used in functional languages as recursive types
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
 }
 
-fn main() {
-    // this isnt really useful
-    // let b = Box::new(5);
-    // println!("b = {}", b);
+impl<T> Deref for MyBox<T> {
+    type Target = T;
 
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y); // *(y.deref())
 }
