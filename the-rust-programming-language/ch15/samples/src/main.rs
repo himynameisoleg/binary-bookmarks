57e5fb80bde20789d2d122050170a08414e80aed
Oleg <heyitsoleg@gmail.com>
2024-11-20 08:40:33 -0600

    Reference Counted Smart Pointers `Rc<T>`

--- the-rust-programming-language/ch15/samples/src/main.rs
+++ the-rust-programming-language/ch15/samples/src/main.rs
@@ -49,21 +49,41 @@
 //     println!("Hello, {}!", name);
 // }
 
-// Code Cleanup with `Drop` trait
-struct CustomSmartPointer {
-    data: String,
+// // Code Cleanup with `Drop` trait
+// struct CustomSmartPointer {
+//     data: String,
+// }
+// impl Drop for CustomSmartPointer {
+//     fn drop(&mut self) {
+//         println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+//     }
+// }
+// fn main() {
+//     let c = CustomSmartPointer {
+//         data: String::from("my stuff"),
+//     };
+//     println!("CustomSmartPointer created.");
+//     // c.drop(); // this doesnt work: explicit destructor calls not allowed
+//     drop(c); // allowed
+//     println!("CustomSmartPointer dropped before end of main.")
+// }
+
+// Sharing Data with Reference Counted Smart Pointer `Rc<T>`
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
 }
-impl Drop for CustomSmartPointer {
-    fn drop(&mut self) {
-        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
-    }
-}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
 fn main() {
-    let c = CustomSmartPointer {
-        data: String::from("my stuff"),
-    };
-    println!("CustomSmartPointer created.");
-    // c.drop(); // this doesnt work: explicit destructor calls not allowed
-    drop(c); // allowed
-    println!("CustomSmartPointer dropped before end of main.")
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+
+    let c = Cons(4, Rc::clone(&a));
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
