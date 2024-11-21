da939c8da85cf590be6276bed4aecb5a11b84031
Oleg <heyitsoleg@gmail.com>
2024-11-21 08:09:49 -0600

    Borrow Rules at runtime with `RefCell<T>`
    - setup Mock Objects usecase

--- the-rust-programming-language/ch15/samples/src/main.rs
+++ the-rust-programming-language/ch15/samples/src/main.rs
@@ -68,22 +68,28 @@
 //     println!("CustomSmartPointer dropped before end of main.")
 // }
 
-// Sharing Data with Reference Counted Smart Pointer `Rc<T>`
-enum List {
-    Cons(i32, Rc<List>),
-    Nil,
-}
+// // Sharing Data with Reference Counted Smart Pointer `Rc<T>`
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil,
+// }
+//
+// use crate::List::{Cons, Nil};
+// use std::rc::Rc;
+//
+// fn main() {
+//     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+//     println!("count after creating a = {}", Rc::strong_count(&a));
+//
+//     let b = Cons(3, Rc::clone(&a));
+//     println!("count after creating b = {}", Rc::strong_count(&a));
+//
+//     let c = Cons(4, Rc::clone(&a));
+//     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+// }
 
-use crate::List::{Cons, Nil};
-use std::rc::Rc;
-
+// Using `RefCell<T>` to check borrow rules at runtime
 fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-
-    let c = Cons(4, Rc::clone(&a));
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    let x = 5;
+    let y = &mut x;
 }
