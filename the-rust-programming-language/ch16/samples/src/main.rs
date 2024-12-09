7cc6e5305f33581257b7602694d47785ce19746c
Oleg <heyitsoleg@gmail.com>
2024-12-09 08:51:33 -0600

    Creating a Mutex

--- the-rust-programming-language/ch16/samples/src/main.rs
+++ the-rust-programming-language/ch16/samples/src/main.rs
@@ -30,42 +30,56 @@
 //     handle.join().unwrap();
 // }
 
-use std::{sync::mpsc, thread, time::Duration};
+// use std::{sync::mpsc, thread, time::Duration};
+//
+// // safely passing messages between Threads
+// fn main() {
+//     let (tx, rx) = mpsc::channel();
+//
+//     let tx1 = mpsc::Sender::clone(&tx);
+//     thread::spawn(move || {
+//         let vals = vec![
+//             String::from("hi"),
+//             String::from("from"),
+//             String::from("the"),
+//             String::from("thread"),
+//         ];
+//
+//         for val in vals {
+//             tx1.send(val).unwrap();
+//             thread::sleep(Duration::from_secs(1));
+//         }
+//     });
+//
+//     thread::spawn(move || {
+//         let vals = vec![
+//             String::from("more"),
+//             String::from("messages"),
+//             String::from("for"),
+//             String::from("you"),
+//         ];
+//
+//         for val in vals {
+//             tx.send(val).unwrap();
+//             thread::sleep(Duration::from_secs(1));
+//         }
+//     });
+//
+//     for received in rx {
+//         println!("Got: {}", received);
+//     }
+// }
 
-// safely passing messages between Threads
+use std::sync::Mutex;
+
+// Using Mutexes
 fn main() {
-    let (tx, rx) = mpsc::channel();
+    let m = Mutex::new(5);
 
-    let tx1 = mpsc::Sender::clone(&tx);
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
-
-        for val in vals {
-            tx1.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("more"),
-            String::from("messages"),
-            String::from("for"),
-            String::from("you"),
-        ];
-
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    for received in rx {
-        println!("Got: {}", received);
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
     }
+
+    println!("m = {:?}", m);
 }
