e6251b23f568aff6dcf7dfb5bd33c7e4b7700030
Oleg <heyitsoleg@gmail.com>
2024-12-19 08:45:42 -0600

    callig external C code or calling Rust from C

--- the-rust-programming-language/ch19/samples/src/main.rs
+++ the-rust-programming-language/ch19/samples/src/main.rs
@@ -1,49 +1,60 @@
-use core::slice;
+// use core::slice;
+//
+// // Unsafe Rust
+// fn main() {
+//     // let mut num = 5;
+//     //
+//     // let r1 = &num as *const i32;
+//     // let r2 = &mut num as *const i32;
+//     //
+//     // // raw pointer with arbitrary memory address
+//     // let address = 0x12345usize;
+//     // let r = address as *const i32;
+//     //
+//     // unsafe {
+//     //     println!("r1 is {}", *r1);
+//     //     println!("r2 is: {}", *r2);
+//     // }
+//     //
+//     // // unsafe functions
+//     // unsafe fn dangerous() {}
+//     //
+//     // unsafe {
+//     //     dangerous();
+//     // }
+//     //
+//     // safe abstraction around Unsafe
+//     let mut v = vec![1, 2, 3, 4, 5, 6];
+//
+//     let r = &mut v[..];
+//
+//     let (a, b) = r.split_at_mut(3);
+//
+//     assert_eq!(a, &mut [1, 2, 3]);
+//     assert_eq!(b, &mut [4, 5, 6]);
+// }
+//
+// fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+//     let len = slice.len();
+//     let ptr = slice.as_mut_ptr();
+//
+//     assert!(mid <= len);
+//
+//     unsafe {
+//         (
+//             slice::from_raw_parts_mut(ptr, mid),
+//             slice::from_raw_parts_mut(ptr.offset(mid as isize), len - mid),
+//         )
+//     }
+// }
 
-// Unsafe Rust
-fn main() {
-    // let mut num = 5;
-    //
-    // let r1 = &num as *const i32;
-    // let r2 = &mut num as *const i32;
-    //
-    // // raw pointer with arbitrary memory address
-    // let address = 0x12345usize;
-    // let r = address as *const i32;
-    //
-    // unsafe {
-    //     println!("r1 is {}", *r1);
-    //     println!("r2 is: {}", *r2);
-    // }
-    //
-    // // unsafe functions
-    // unsafe fn dangerous() {}
-    //
-    // unsafe {
-    //     dangerous();
-    // }
-    //
-    // safe abstraction around Unsafe
-    let mut v = vec![1, 2, 3, 4, 5, 6];
-
-    let r = &mut v[..];
-
-    let (a, b) = r.split_at_mut(3);
-
-    assert_eq!(a, &mut [1, 2, 3]);
-    assert_eq!(b, &mut [4, 5, 6]);
+// Using `extern` to call external code
+extern "C" {
+    fn abs(input: i32) -> i32;
 }
 
-fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
-    let len = slice.len();
-    let ptr = slice.as_mut_ptr();
-
-    assert!(mid <= len);
-
+fn main() {
     unsafe {
-        (
-            slice::from_raw_parts_mut(ptr, mid),
-            slice::from_raw_parts_mut(ptr.offset(mid as isize), len - mid),
-        )
+        println!("abs value of -3 according to C: {}", abs(-3));
     }
 }
